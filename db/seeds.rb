c796c287d09c95f0026197c12a1942727cb6606b
rnere <rushi@nonstopio.com>
2024-07-30 11:15:49 +0530

    Few more enhancements

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,90 +8,90 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # Create a sample user
-User.create!(email: 'rushi@example.com', password: 'password', password_confirmation: 'password')
+# User.create!(email: 'rushi@example.com', password: 'password', password_confirmation: 'password')
 
 # Create a sample post
-post = Post.create!(
-  title: 'Sample Post 1',
-  author: 'Admin',
-  content: 'This is a first sample post content.',
-  status: 'published',
-  creation_date: Time.now,
-  published_date: Time.now
-)
+# post = Post.create!(
+#   title: 'Sample Post 1',
+#   author: 'Admin',
+#   content: 'This is a first sample post content.',
+#   status: 'published',
+#   creation_date: Time.now,
+#   published_date: Time.now
+# )
 
-post = Post.create!(
-  title: 'Sample Post 2',
-  author: 'Other Admin',
-  content: 'This is a second sample post content.',
-  status: 'published',
-  creation_date: Time.now,
-  published_date: Time.now
-)
+# post = Post.create!(
+#   title: 'Sample Post 2',
+#   author: 'Other Admin',
+#   content: 'This is a second sample post content.',
+#   status: 'published',
+#   creation_date: Time.now,
+#   published_date: Time.now
+# )
 
-post = Post.create!(
-  title: 'Sample Post 3',
-  author: 'Admin',
-  content: 'This is a third sample post content.',
-  status: 'published',
-  creation_date: Time.now,
-  published_date: Time.now
-)
+# post = Post.create!(
+#   title: 'Sample Post 3',
+#   author: 'Admin',
+#   content: 'This is a third sample post content.',
+#   status: 'published',
+#   creation_date: Time.now,
+#   published_date: Time.now
+# )
 
-post = Post.create!(
-  title: 'Sample Post 4',
-  author: 'Admin',
-  content: 'This is a fourth sample post content.',
-  status: 'published',
-  creation_date: Time.now,
-  published_date: Time.now
-)
+# post = Post.create!(
+#   title: 'Sample Post 4',
+#   author: 'Admin',
+#   content: 'This is a fourth sample post content.',
+#   status: 'published',
+#   creation_date: Time.now,
+#   published_date: Time.now
+# )
 
-post = Post.create!(
-  title: 'Sample Post 5',
-  author: 'Admin',
-  content: 'This is a fifth sample post content.',
-  status: 'published',
-  creation_date: Time.now,
-  published_date: Time.now
-)
+# post = Post.create!(
+#   title: 'Sample Post 5',
+#   author: 'Admin',
+#   content: 'This is a fifth sample post content.',
+#   status: 'published',
+#   creation_date: Time.now,
+#   published_date: Time.now
+# )
 
-post = Post.create!(
-  title: 'Sample Post 6',
-  author: 'Admin',
-  content: 'This is a sixth sample post content.',
-  status: 'published',
-  creation_date: Time.now,
-  published_date: Time.now
-)
+# post = Post.create!(
+#   title: 'Sample Post 6',
+#   author: 'Admin',
+#   content: 'This is a sixth sample post content.',
+#   status: 'published',
+#   creation_date: Time.now,
+#   published_date: Time.now
+# )
 
-# Create a sample comment
-Comment.create!(
-  name: 'Commenter 1',
-  text: 'This is a first sample comment.',
-  post: post
-)
+# # Create a sample comment
+# Comment.create!(
+#   name: 'Commenter 1',
+#   text: 'This is a first sample comment.',
+#   post: post
+# )
 
-Comment.create!(
-  name: 'Commenter 2',
-  text: 'This is a second sample comment.',
-  post: post
-)
+# Comment.create!(
+#   name: 'Commenter 2',
+#   text: 'This is a second sample comment.',
+#   post: post
+# )
 
-Comment.create!(
-  name: 'Commenter 3',
-  text: 'This is a third sample comment.',
-  post: post
-)
+# Comment.create!(
+#   name: 'Commenter 3',
+#   text: 'This is a third sample comment.',
+#   post: post
+# )
 
-Comment.create!(
-  name: 'Commenter 4',
-  text: 'This is a fourth sample comment.',
-  post: post
-)
+# Comment.create!(
+#   name: 'Commenter 4',
+#   text: 'This is a fourth sample comment.',
+#   post: post
+# )
 
-Comment.create!(
-  name: 'Commenter 1',
-  text: 'This is a fifth sample comment.',
-  post: post
-)
+# Comment.create!(
+#   name: 'Commenter 1',
+#   text: 'This is a fifth sample comment.',
+#   post: post
+# )
